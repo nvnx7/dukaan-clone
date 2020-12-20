b9c2575875bc93d1b9c3e52a7d144a22e50c8330
Naveen <thenvnsahu@gmail.com>
2020-12-20 16:47:03 +0530

    added consistent related_name for models

--- backend/shop/models/product.py
+++ backend/shop/models/product.py
@@ -27,13 +27,13 @@ class Product(models.Model):
     price = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, validators=[MinValueValidator(0.01)])
     category = models.ForeignKey(
-        ProductCategory, on_delete=models.CASCADE, default=1, related_name='product_category')
+        ProductCategory, on_delete=models.CASCADE, default=1, related_name='category_products')
     image = models.ImageField(upload_to='uploads/products/')
     stock = models.IntegerField(
         validators=[MinValueValidator(0), MaxValueValidator(9999999999)])
     unit = CharField(max_length=2, choices=UNITS)
     shop = models.ForeignKey(
-        Shop, on_delete=models.CASCADE, related_name='product_shop')
+        Shop, on_delete=models.CASCADE, related_name='shop_products')
 
     def __str__(self) -> str:
         return self.title
@@ -49,9 +49,9 @@ class Order(models.Model):
     quantity = models.IntegerField(null=False, validators=[
                                    MinValueValidator(1), MaxValueValidator(9999999999)])
     product = models.ForeignKey(
-        Product, on_delete=models.CASCADE, related_name='order_product')
+        Product, on_delete=models.CASCADE, related_name='product_orders')
     delivery_address = models.CharField(max_length=500, null=False)
     customer = models.ForeignKey(
-        Customer, on_delete=models.CASCADE, related_name='order_customer')
+        Customer, on_delete=models.CASCADE, related_name='customer_orders')
     datetime = models.DateTimeField(default=datetime.datetime.now)
     status = models.IntegerField(choices=ORDER_STATUS)
--- backend/shop/models/shop.py
+++ backend/shop/models/shop.py
@@ -15,6 +15,7 @@ class ShopOwner(models.Model):
 class Shop(models.Model):
     title = models.CharField(max_length=100, null=False)
     address = models.CharField(max_length=500, null=False)
-    owner = models.ForeignKey(ShopOwner, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        ShopOwner, on_delete=models.CASCADE, related_name='owner_shops')
     revenue = models.DecimalField(max_digits=12, decimal_places=2, default=0)
     created = models.DateTimeField(default=datetime.datetime.now)
