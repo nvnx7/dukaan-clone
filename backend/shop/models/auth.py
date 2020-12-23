054da2276c1e06fb292413253797cc1a830b4e57
Naveen <thenvnsahu@gmail.com>
2020-12-24 00:24:35 +0530

    custom user model

---
+++ backend/shop/models/auth.py
@@ -0,0 +1,53 @@
+
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+from django.utils import timezone
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, phone, password, **extra_fields):
+        """
+        Create and save a User with the given phone and password.
+        """
+        if not phone:
+            raise ValueError(_('The Phone must be set'))
+        user = self.model(phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, phone, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given phone and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(phone, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=100, default='')
+    last_name = models.CharField(max_length=100, default='')
+    phone = models.IntegerField(null=False, unique=True)
+    date_joined = models.DateTimeField(
+        default=timezone.now)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'date_joined']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return str(self.phone)
