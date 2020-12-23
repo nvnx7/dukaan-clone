f7c95e6a23c5638fd4aed242fc0ecc43226da3cc
Naveen <thenvnsahu@gmail.com>
2020-12-24 00:25:08 +0530

    refactor: use User model

--- backend/shop/models/shop.py
+++ backend/shop/models/shop.py
@@ -1,22 +1,13 @@
+from .auth import User
 from django.db import models
-from django.core.validators import MinLengthValidator, MaxLengthValidator
 import datetime
-
-
-class ShopOwner(models.Model):
-    first_name = models.CharField(max_length=100, default='')
-    last_name = models.CharField(max_length=100, default='')
-    phone = models.IntegerField(null=False, unique=True)
-    password = models.CharField(max_length=128, null=False)
-    joined = models.DateTimeField(
-        default=datetime.datetime.now)
 
 
 class Shop(models.Model):
     title = models.CharField(max_length=100, null=False)
     address = models.CharField(max_length=500, null=False)
     owner = models.ForeignKey(
-        ShopOwner, on_delete=models.CASCADE, related_name='owner_shops')
+        User, on_delete=models.CASCADE, related_name='owner_shops')
     revenue = models.DecimalField(max_digits=12, decimal_places=2, default=0)
     created = models.DateTimeField(
         default=datetime.datetime.now)
