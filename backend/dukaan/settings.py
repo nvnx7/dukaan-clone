fbe09c109068e7e597e92bf2f1a031fff68fd762
Naveen <thenvnsahu@gmail.com>
2020-12-24 20:05:38 +0530

    remove default permissions

--- backend/dukaan/settings.py
+++ backend/dukaan/settings.py
@@ -113,6 +113,10 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        # 'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
