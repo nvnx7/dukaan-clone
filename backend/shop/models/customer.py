b32baf99135fd71143b81cd6ae6f1bfe168474b0
Naveen <thenvnsahu@gmail.com>
2020-12-26 03:27:55 +0530

    refactor

--- backend/shop/models/customer.py
+++ backend/shop/models/customer.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils import timezone
 
@@ -9,17 +8,16 @@ from .product import Product
 class Customer(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    phone = models.IntegerField(null=False, validators=[
-                                MinLengthValidator(4), MaxLengthValidator(12)])
-    address = models.CharField(max_length=500, null=False)
+    phone = models.IntegerField(null=False)
+    # address = models.CharField(max_length=500, null=False)
 
 
 class Order(models.Model):
     ORDER_STATUS = (
-        (0, 'pending'),
-        (1, 'accepted'),
-        (2, 'shipped'),
-        (3, 'delivered')
+        (1, 'pending'),
+        (2, 'accepted'),
+        (3, 'shipped'),
+        (4, 'delivered')
     )
     quantity = models.IntegerField(null=False, validators=[
                                    MinValueValidator(1), MaxValueValidator(9999999999)])
@@ -29,4 +27,4 @@ class Order(models.Model):
     customer = models.ForeignKey(
         Customer, on_delete=models.CASCADE, related_name='customer_orders')
     date_ordered = models.DateTimeField(default=timezone.now)
-    status = models.IntegerField(choices=ORDER_STATUS)
+    status = models.IntegerField(choices=ORDER_STATUS, default=1)
