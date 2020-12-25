168fee1845f21c1b144e419bf02b233455e48d35
Naveen <thenvnsahu@gmail.com>
2020-12-26 03:31:01 +0530

    refactored urls

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -1,17 +1,9 @@
 from django.urls import path
 from .views.root_view import api_root
-from .views import shop_views
-from .views import auth_views
+from .views import auth_views, shop_views, product_views, customer_views
 
 from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework.authtoken.views import obtain_auth_token
-
-shop_detail = shop_views.ShopViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
+# from rest_framework.authtoken.views import obtain_auth_token
 
 owner_detail = shop_views.ShopOwnerViewSet.as_view({
     'get': 'retrieve',
@@ -22,7 +14,43 @@ owner_detail = shop_views.ShopOwnerViewSet.as_view({
 
 shop_list = shop_views.ShopViewSet.as_view({
     'get': 'list',
+    'post': 'create',
+})
+
+shop_detail = shop_views.ShopViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+product_list = product_views.ProductViewSet.as_view({
+    'get': 'list',
     'post': 'create'
+})
+
+product_detail = product_views.ProductViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+# customer_list = customer_views.CustomerViewSet.as_view({
+#     'post': 'create'
+# })
+
+# customer_detail = customer_views.CustomerViewSet.as_view({
+#     'get': 'retrieve'
+# })
+
+order_list = customer_views.OrderViewSet.as_view({
+    'get': 'list',
+    'post': 'create_multiple'
+})
+
+order_detail = customer_views.OrderViewSet.as_view({
+    'get': 'retrieve'
 })
 
 urlpatterns = [
@@ -30,13 +58,21 @@ urlpatterns = [
 
     path('signup/', auth_views.SignUpView.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('login/authtoken/', obtain_auth_token, name='owner-login'),
+
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('locked/', auth_views.LockedView.as_view(), name='locked'),
 
-    path('shop/<int:pk>/', shop_detail, name='shop-detail'),
     path('shopowner/<int:pk>/', owner_detail, name='owner-detail'),
-    path('shopowner/<int:pk>/shops/', shop_list, name='shop-list')
+    path('shopowner/<int:owner_id>/shops/', shop_list, name='shop-list'),
+
+    path('shop/<int:pk>/', shop_detail, name='shop-detail'),
+    path('shop/<int:pk>/products/', product_list, name='product-list'),
+    path('shop/<int:shop_id>/orders/', order_list, name='order-list'),
+
+    path('product/<int:pk>/', product_detail, name='product-detail'),
+
+    # path('customer/<int:pk>/', customer_detail, name='customer-detail'),
+    # path('customers/', customer_list, name='customer-list'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
