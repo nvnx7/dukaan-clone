590993d1d77f4a30b5384819cb54e00f28461162
Naveen <thenvnsahu@gmail.com>
2020-12-26 03:29:31 +0530

    custom list method

--- backend/shop/views/shop_views.py
+++ backend/shop/views/shop_views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import status
 
 from ..models.shop import Shop
 from ..models.auth import User
@@ -23,6 +24,16 @@ class ShopViewSet(viewsets.ModelViewSet):
 
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     authentication_classes = [TokenAuthentication]
+
+    def list(self, request, *args, **kwargs):
+        """
+        Filter using the owner id & return shop details
+        """
+        owner_id = kwargs['owner_id']
+        shops = self.queryset.filter(owner_id=owner_id)
+        serializer = ShopSerializer(
+            instance=shops, many=True, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
