c839b8ad438425d7761fd38cf471543f148e5d0a
Naveen <thenvnsahu@gmail.com>
2020-12-30 03:56:57 +0530

    redux store with combined reducers

---
+++ frontend/src/reducers/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+import authReducer from "./authReducers";
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+export default rootReducer;
---
+++ frontend/src/store.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import rootReducer from "./reducers/rootReducer";
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+
+export default store;
