bb46f03886f8edd0bd9f1d6b98c2f8dc2313c550
Naveen <thenvnsahu@gmail.com>
2020-12-30 03:54:16 +0530

    refactor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,50 @@
+import { Provider } from "react-redux";
+
+import { Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import { Switch, Route } from "react-router-dom";
+
+import store from "./store";
+
 import Header from "./components/Header";
-import LoginForm from "./components/LoginForm";
-import SignUpForm from "./components/SignUpForm";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  content: {
+    flexGrow: 1,
+  },
+  header: {
+    // flexGrow: 1,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="root">
-      <Header />
-      {/* <LoginForm /> */}
-      <SignUpForm />
-    </div>
+    <Provider store={store}>
+      <Box className={classes.root} container direction="column">
+        <Box className={classes.header} item>
+          <Header />
+        </Box>
+
+        <Box className={classes.content} item>
+          <Switch>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Box>
+      </Box>
+    </Provider>
   );
 }
 
