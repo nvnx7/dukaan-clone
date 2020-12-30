d27d6f2f34fcf171f1408f229627ada59cbb415d
Naveen <thenvnsahu@gmail.com>
2020-12-30 20:31:53 +0530

    selector for loading indicator

--- frontend/src/selectors/authSelectors.js
+++ frontend/src/selectors/authSelectors.js
@@ -2,6 +2,11 @@ import { createSelector } from "reselect";
 
 const userSelector = (state) => state.auth.user;
 const authErrorSelector = (state) => state.auth.error;
+const authLoadingSelector = (state) => state.auth.loading;
 
 export const userInfo = createSelector([userSelector], (user) => user);
 export const authError = createSelector([authErrorSelector], (error) => error);
+export const authLoading = createSelector(
+  [authLoadingSelector],
+  (loading) => loading
+);
