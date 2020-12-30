325821c4d8993b331a149488a1d26f88bc8290e9
Naveen <thenvnsahu@gmail.com>
2020-12-30 20:29:36 +0530

    register & logout reducers

--- frontend/src/reducers/authReducers.js
+++ frontend/src/reducers/authReducers.js
@@ -2,12 +2,20 @@ import {
   AUTH_LOGIN_REQUEST,
   AUTH_LOGIN_SUCCESS,
   AUTH_LOGIN_FAILURE,
+  AUTH_REGISTER_REQUEST,
+  AUTH_REGISTER_SUCCESS,
+  AUTH_REGISTER_FAILURE,
+  AUTH_LOGOUT_REQUEST,
+  AUTH_LOGOUT_SUCCESS,
+  AUTH_LOGOUT_FAILURE,
   AUTH_ERROR_HIDE,
 } from "../actions/authActions";
 
 const initialState = {
   loading: false,
-  user: {},
+  user: {
+    authenticated: false,
+  },
   error: "",
 };
 
@@ -19,6 +27,20 @@ const authReducer = (state = initialState, action) => {
       return { ...state, loading: false, user: action.payload, error: "" };
     case AUTH_LOGIN_FAILURE:
       return { ...state, loading: false, user: "", error: action.payload };
+
+    case AUTH_REGISTER_REQUEST:
+      return { ...state, loading: true };
+    case AUTH_REGISTER_SUCCESS:
+      return { ...state, loading: false, user: action.payload, error: "" };
+    case AUTH_REGISTER_FAILURE:
+      return { ...state, loading: false, user: "", error: action.payload };
+
+    case AUTH_LOGOUT_REQUEST:
+      return { ...state, loading: true };
+    case AUTH_LOGOUT_SUCCESS:
+      return initialState;
+    case AUTH_LOGOUT_FAILURE:
+      return { ...state, loading: false, error: action.payload };
 
     case AUTH_ERROR_HIDE:
       return { ...state, error: "" };
