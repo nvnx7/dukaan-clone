609dc88bffc2456dd9d4dd5bead4b6a73d1f5a1d
Naveen <thenvnsahu@gmail.com>
2021-01-04 04:35:16 +0530

    combine reducers

--- frontend/src/reducers/rootReducer.js
+++ frontend/src/reducers/rootReducer.js
@@ -1,9 +1,13 @@
 import { combineReducers } from "redux";
 
 import authReducer from "./authReducers";
+import dashboardReducer from "./dashboardReducers";
+import shopReducer from "./shopReducers";
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  dashboard: dashboardReducer,
+  shop: shopReducer,
 });
 
 export default rootReducer;
