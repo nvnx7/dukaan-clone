da21db17d069a1f425b3f12624e68eefe47ba9ef
Naveen <thenvnsahu@gmail.com>
2021-01-04 00:32:33 +0530

    refactor: return null if file is non truthy value

--- frontend/src/utils/imageUtils.js
+++ frontend/src/utils/imageUtils.js
@@ -2,6 +2,8 @@ export function readAsDataUrl(file) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
 
+    if (!file) resolve(null);
+
     reader.onload = () => {
       resolve(reader.result);
     };
