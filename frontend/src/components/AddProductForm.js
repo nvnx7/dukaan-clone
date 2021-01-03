93a04c161b30341547bd8069fe9663f252ff6731
Naveen <thenvnsahu@gmail.com>
2021-01-03 19:42:35 +0530

    forms to add/edit products

---
+++ frontend/src/components/AddProductForm.js
@@ -0,0 +1,269 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  TextField,
+  CssBaseline,
+  Container,
+  Grid,
+  Button,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+} from "@material-ui/core";
+
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+import UploadButton from "./UploadButton";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+  actionBtn: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const MAX_FILE_SIZE = 1 * 1024 * 1024;
+const SUPPORTED_TYPES = ["image/jpg", "image/jpeg", "image/gif", "image/png"];
+
+const validationSchema = yup.object({
+  title: yup.string().required("Required!").min(1),
+  description: yup.string(),
+  price: yup
+    .number("Enter price")
+    .typeError("Invalid price!")
+    .required("Price is required!")
+    .positive("Invalid price!"),
+  stock: yup
+    .number("Enter number of stocks!")
+    .typeError("Invalid number of stocks!")
+    .required("Stocks is required!")
+    .positive("Invalid number of stocks!"),
+  unit: yup.string().required("Unit is required!"),
+  category: yup.string().required("Unit is required!"),
+});
+
+export default function AddProductForm({ loading, onSave, onCancel }) {
+  const classes = useStyles();
+
+  const [file, setFile] = useState(null);
+  const [fileError, setFileError] = useState("");
+  const validateFile = (file) => {
+    if (!file) return "Image File Required!";
+    if (file && file.size > MAX_FILE_SIZE) return "File Too Large!";
+    if (file && !SUPPORTED_TYPES.includes(file.type))
+      return "File not supported!";
+    return "";
+  };
+
+  const handleOnChange = (file) => {
+    const error = validateFile(file);
+    if (error) {
+      setFile(null);
+      setFileError(error);
+      return;
+    }
+
+    setFileError("");
+    setFile(file);
+  };
+
+  const onSubmit = (values) => {
+    console.log("SUBMITTING");
+    const error = validateFile(file);
+    if (error) {
+      setFile(null);
+      setFileError(error);
+      return;
+    }
+    const vals = { ...values, image: file.name };
+    alert(JSON.stringify(vals));
+    // onSave(vals);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      description: "",
+      price: "",
+      stock: "",
+      unit: "",
+      category: "",
+    },
+    validationSchema,
+    onSubmit,
+  });
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <form className={classes.form} onSubmit={formik.handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="title"
+              name="title"
+              label="Title"
+              value={formik.values.title}
+              onChange={formik.handleChange}
+              error={formik.touched.title && Boolean(formik.errors.title)}
+              helperText={formik.touched.title && formik.errors.title}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              multiline
+              fullWidth
+              rows={8}
+              id="description"
+              name="description"
+              label="Description"
+              value={formik.values.description}
+              onChange={formik.handleChange}
+              error={
+                formik.touched.description && Boolean(formik.errors.description)
+              }
+              helperText={
+                formik.touched.description && formik.errors.description
+              }
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <UploadButton
+              name="file"
+              label="Upload Product Pic"
+              onChange={handleOnChange}
+              file={file}
+              error={fileError}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="price"
+              name="price"
+              label="Price per unit"
+              value={formik.values.price}
+              onChange={formik.handleChange}
+              error={formik.touched.price && Boolean(formik.errors.price)}
+              helperText={formik.touched.price && formik.errors.price}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <FormControl variant="outlined" fullWidth>
+              <InputLabel id="unit-label">Unit</InputLabel>
+              <Select
+                variant="outlined"
+                margin="normal"
+                id="unit"
+                name="unit"
+                label="Unit"
+                labelId="unit-label"
+                value={formik.values.unit}
+                onChange={formik.handleChange}
+                error={formik.touched.unit && Boolean(formik.errors.unit)}
+                helperText={formik.touched.unit && formik.errors.unit}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={"pc"}>Piece</MenuItem>
+                <MenuItem value={"kg"}>kilogram</MenuItem>
+                <MenuItem value={"g"}>gram</MenuItem>
+                <MenuItem value={"l"}>litre</MenuItem>
+                <MenuItem value={"ml"}>millilitre</MenuItem>
+                <MenuItem value={"dz"}>dozen</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="stock"
+              name="stock"
+              label="Stock"
+              value={formik.values.stock}
+              onChange={formik.handleChange}
+              error={formik.touched.stock && Boolean(formik.errors.stock)}
+              helperText={formik.touched.stock && formik.errors.stock}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <FormControl variant="outlined" fullWidth>
+              <InputLabel id="category-label">Category</InputLabel>
+              <Select
+                variant="outlined"
+                margin="normal"
+                id="category"
+                name="category"
+                label="Category"
+                labelId="category-label"
+                value={formik.values.category}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.category && Boolean(formik.errors.category)
+                }
+                helperText={formik.touched.category && formik.errors.category}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value="1">Grocery</MenuItem>
+                <MenuItem value="2">Fast Food</MenuItem>
+                <MenuItem value="3">Garments</MenuItem>
+                <MenuItem value="4">Stationary</MenuItem>
+                {/* <MenuItem value="5">millilitre</MenuItem>
+              <MenuItem value="6">dozen</MenuItem> */}
+              </Select>
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.actionBtn}
+              disabled={loading}
+            >
+              Save
+            </Button>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Button
+              fullWidth
+              color="primary"
+              className={classes.actionBtn}
+              onClick={onCancel}
+              disabled={loading}
+            >
+              Cancel
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
+}
---
+++ frontend/src/components/EditProductForm.js
@@ -0,0 +1,283 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  TextField,
+  CssBaseline,
+  Container,
+  Grid,
+  Button,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+} from "@material-ui/core";
+
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+import UploadButton from "./UploadButton";
+
+const useStyles = makeStyles((theme) => ({
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+  actionBtn: {
+    margin: theme.spacing(2, 0, 2),
+    float: "right",
+  },
+}));
+
+const MAX_FILE_SIZE = 1 * 1024 * 1024;
+const SUPPORTED_TYPES = ["image/jpg", "image/jpeg", "image/gif", "image/png"];
+
+const validationSchema = yup.object({
+  id: yup.number().required(),
+  title: yup.string().required("Required!").min(1),
+  description: yup.string(),
+  price: yup
+    .number("Enter price")
+    .typeError("Invalid price!")
+    .required("Price is required!")
+    .positive("Invalid price!"),
+  stock: yup
+    .number("Enter number of stocks!")
+    .typeError("Invalid number of stocks!")
+    .required("Stocks is required!")
+    .positive("Invalid number of stocks!"),
+  unit: yup.string().required("Unit is required!"),
+  category: yup.string().required("Unit is required!"),
+  // file: yup
+  //   .mixed()
+  //   // .required("A file is required")
+  //   .test("fileSize", "File too large", (value) => {
+  //     console.log("FILE SIZE!!" + value);
+  //     if (value && value.size > FILE_SIZE) return false;
+  //     return true;
+  //     // return value && value.size <= FILE_SIZE;
+  //   })
+  //   .test("fileFormat", "Unsupported Format", (value) => {
+  //     // const b = value && SUPPORTED_FORMATS.includes(value.type);
+  //     console.log("FILE FORMAT!!" + value.type);
+  //     if (value && !SUPPORTED_FORMATS.includes(value.type)) return false;
+  //     return true;
+  //     // return value && SUPPORTED_FORMATS.includes(value.type);
+  //   }),
+});
+
+export default function EditProductForm({
+  product,
+  loading,
+  onSave,
+  onCancel,
+}) {
+  const classes = useStyles();
+
+  const [file, setFile] = useState(null);
+  const [fileError, setFileError] = useState("");
+
+  const validateFile = (file) => {
+    if (file && file.size > MAX_FILE_SIZE) return "File Too Large!";
+    if (file && !SUPPORTED_TYPES.includes(file.type))
+      return "File not supported!";
+    return "";
+  };
+
+  const handleOnChange = (file) => {
+    const error = validateFile(file);
+    if (error) {
+      setFile(null);
+      setFileError(error);
+      return;
+    }
+
+    setFileError("");
+    setFile(file);
+  };
+
+  const onSubmit = (values) => {
+    const vals = { ...values, image: file };
+    alert(JSON.stringify(vals));
+    // onSave(vals);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      id: product ? product.id : "",
+      title: product ? product.title : "",
+      description: product ? product.description : "",
+      price: product ? product.price : "",
+      stock: product ? product.stock : "",
+      unit: product ? product.unit : "",
+      category: product ? product.category : "",
+      // file: null,
+    },
+    validationSchema,
+    onSubmit,
+  });
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <form className={classes.form} onSubmit={formik.handleSubmit}>
+        <input type="hidden" id="id" name="id" value={formik.values.id}></input>
+
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="title"
+              name="title"
+              label="Title"
+              value={formik.values.title}
+              onChange={formik.handleChange}
+              error={formik.touched.title && Boolean(formik.errors.title)}
+              helperText={formik.touched.title && formik.errors.title}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              multiline
+              fullWidth
+              rows={8}
+              id="description"
+              name="description"
+              label="Description"
+              value={formik.values.description}
+              onChange={formik.handleChange}
+              error={
+                formik.touched.description && Boolean(formik.errors.description)
+              }
+              helperText={
+                formik.touched.description && formik.errors.description
+              }
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <UploadButton
+              name="file"
+              label="Upload Product Pic"
+              onChange={handleOnChange}
+              file={file}
+              error={fileError}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="price"
+              name="price"
+              label="Price per unit"
+              value={formik.values.price}
+              onChange={formik.handleChange}
+              error={formik.touched.price && Boolean(formik.errors.price)}
+              helperText={formik.touched.price && formik.errors.price}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <FormControl variant="outlined" fullWidth>
+              <InputLabel id="unit-label">Unit</InputLabel>
+              <Select
+                variant="outlined"
+                margin="normal"
+                id="unit"
+                name="unit"
+                label="Unit"
+                labelId="unit-label"
+                value={formik.values.unit}
+                onChange={formik.handleChange}
+                error={formik.touched.unit && Boolean(formik.errors.unit)}
+                helperText={formik.touched.unit && formik.errors.unit}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={"pc"}>Piece</MenuItem>
+                <MenuItem value={"kg"}>kilogram</MenuItem>
+                <MenuItem value={"g"}>gram</MenuItem>
+                <MenuItem value={"l"}>litre</MenuItem>
+                <MenuItem value={"ml"}>millilitre</MenuItem>
+                <MenuItem value={"dz"}>dozen</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="stock"
+              name="stock"
+              label="Stock"
+              value={formik.values.stock}
+              onChange={formik.handleChange}
+              error={formik.touched.stock && Boolean(formik.errors.stock)}
+              helperText={formik.touched.stock && formik.errors.stock}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <FormControl variant="outlined" fullWidth>
+              <InputLabel id="category-label">Category</InputLabel>
+              <Select
+                variant="outlined"
+                margin="normal"
+                id="category"
+                name="category"
+                label="Category"
+                labelId="category-label"
+                value={formik.values.category}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.category && Boolean(formik.errors.category)
+                }
+                helperText={formik.touched.category && formik.errors.category}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value="1">Grocery</MenuItem>
+                <MenuItem value="2">Fast Food</MenuItem>
+                <MenuItem value="3">Garments</MenuItem>
+                <MenuItem value="4">Stationary</MenuItem>
+                {/* <MenuItem value="5">millilitre</MenuItem>
+              <MenuItem value="6">dozen</MenuItem> */}
+              </Select>
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.actionBtn}
+              disabled={loading}
+            >
+              Save
+            </Button>
+          </Grid>
+
+          <Grid item xs={4}>
+            <Button
+              fullWidth
+              color="primary"
+              className={classes.actionBtn}
+              onClick={onCancel}
+              disabled={loading}
+            >
+              Cancel
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
+}
