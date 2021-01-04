42906b53db47da73efc13c983645c25857ebf718
Naveen <thenvnsahu@gmail.com>
2021-01-05 01:56:29 +0530

    custom retrieve method

--- backend/shop/views/shop_views.py
+++ backend/shop/views/shop_views.py
@@ -6,7 +6,9 @@ from rest_framework import status
 
 from ..models.shop import Shop
 from ..models.auth import User
+from ..models.customer import Order
 from ..serializers.shop_serializers import ShopSerializer, ShopOwnerSerializer
+from ..serializers.customer_serializers import OrderSerializer
 from ..permissions import IsOwnerOrReadOnly
 
 
@@ -24,6 +26,25 @@ class ShopViewSet(viewsets.ModelViewSet):
 
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     authentication_classes = [TokenAuthentication]
+
+    def retrieve(self, request, *args, **kwargs):
+        shop_id = kwargs['pk']
+        shop = self.queryset.get(pk=shop_id)
+        shopSerializer = ShopSerializer(
+            instance=shop, context={'request': request})
+
+        # If user is not authenticated owner of shop send only shop & products details
+        if not request.user.is_authenticated:
+            return Response(shopSerializer.data, status=status.HTTP_200_OK)
+
+        # Else include all order details to this shop
+        orders = Order.objects.filter(shop=shop_id)
+        ordersSerializer = OrderSerializer(
+            instance=orders, many=True, context={'request': request})
+
+        response = dict(shopSerializer.data)
+        response['orders'] = list(ordersSerializer.data)
+        return Response(response, status=status.HTTP_200_OK)
 
     def list(self, request, *args, **kwargs):
         """
