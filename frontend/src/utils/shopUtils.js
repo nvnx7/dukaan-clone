453e177ab1864a1a7bfc40157550eecc48b8a4f5
Naveen <thenvnsahu@gmail.com>
2021-01-05 23:39:54 +0530

    util functions

--- frontend/src/utils/shopUtils.js
+++ frontend/src/utils/shopUtils.js
@@ -14,3 +14,33 @@ export function addProductInShop(shopDetail, addedProduct) {
   updatedShopDetails["shop_products"].push(addedProduct);
   return updatedShopDetails;
 }
+
+export function getActiveOrdersCount(shopDetail) {
+  if (!shopDetail.orders) return 0;
+  return shopDetail.orders.reduce((total, order) => {
+    if (order.status !== 4) return total + 1;
+    return total;
+  }, 0);
+}
+
+export function getPendingOrdersCount(shopDetail) {
+  if (!shopDetail.orders) return 0;
+  return shopDetail.orders.reduce((total, order) => {
+    if (order.status === 1) return total + 1;
+    return total;
+  }, 0);
+}
+
+export function getProductsCount(shopDetail) {
+  if (!shopDetail["shop_products"]) return 0;
+  console.log(`shopDetail["shop_products"].length`);
+  return shopDetail["shop_products"].length;
+}
+
+export function getOutOfStockProductsCount(shopDetail) {
+  if (!shopDetail["shop_products"]) return 0;
+  return shopDetail["shop_products"].reduce((total, product) => {
+    if (product.stock === 0) return total + 1;
+    else return total;
+  }, 0);
+}
