ee4bd5a24b9eb1716a44e1723a7620e97f80b217
Naveen <thenvnsahu@gmail.com>
2021-01-07 17:15:24 +0530

    added url field

--- backend/shop/serializers/shop_serializers.py
+++ backend/shop/serializers/shop_serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from rest_framework.relations import HyperlinkedRelatedField
+from rest_framework.relations import HyperlinkedIdentityField, HyperlinkedRelatedField
 from ..models.shop import Shop
 from ..models.auth import User
 from ..serializers.product_serializers import ProductSerializer
@@ -7,12 +7,13 @@ from ..serializers.product_serializers import ProductSerializer
 
 class ShopSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.ReadOnlyField()
+    url = serializers.HyperlinkedIdentityField(view_name='shop-detail')
     owner = HyperlinkedRelatedField(view_name='owner-detail', read_only=True)
     shop_products = ProductSerializer(many=True, read_only=True)
 
     class Meta:
         model = Shop
-        fields = ['id', 'title', 'address', 'date_created',
+        fields = ['id', 'url', 'title', 'address', 'date_created',
                   'revenue', 'owner', 'shop_products']
 
         read_only_fields = ['date_created']
