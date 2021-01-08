b278093b74e58f50e1c413b8da223c2769847ed5
Naveen <thenvnsahu@gmail.com>
2021-01-08 20:11:31 +0530

    changed order of middleware

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,6 +3,6 @@ import thunk from "redux-thunk";
 import logger from "redux-logger";
 import rootReducer from "./reducers/rootReducer";
 
-const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 export default store;
