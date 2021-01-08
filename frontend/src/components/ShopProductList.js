6f0cbb38a6a4b3a000a4efe1bd5e1402b5b95907
Naveen <thenvnsahu@gmail.com>
2021-01-08 20:09:20 +0530

    components to list products for customer

---
+++ frontend/src/components/ShopProductList.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Typography, Box } from "@material-ui/core";
+
+import ShopProductListTile from "./ShopProductListTile";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100%",
+  },
+  itemList: {
+    height: "100%",
+    maxHeight: "550px",
+    overflowY: "scroll",
+  },
+}));
+
+export default function ShopProductList({ productsData, onChangeCount }) {
+  const classes = useStyles();
+
+  return (
+    <Box
+      className={classes.root}
+      p={1}
+      display="flex"
+      flexDirection="column"
+      alignItems="stretch"
+      justifyContent="flex-start"
+    >
+      <Box p={1}>
+        <Typography>{`${productsData.length} Products`}</Typography>
+      </Box>
+
+      {productsData.length > 0 ? (
+        <Grid
+          className={classes.itemList}
+          container
+          spacing={2}
+          alignItems="flex-start"
+          justify="flex-start"
+        >
+          {productsData.map((product) => (
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <ShopProductListTile
+                id={product.id}
+                key={product.image}
+                title={product.title}
+                description={product.description}
+                imageSrc={product.image}
+                price={product.price}
+                stock={product.stock}
+                unit={product.unit}
+                category={product.category}
+                count={product.count}
+                onChangeCount={onChangeCount}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      ) : (
+        <Typography variant="body2" color="textSecondary" align="center">
+          Shop is empty!
+        </Typography>
+      )}
+    </Box>
+  );
+}
---
+++ frontend/src/components/ShopProductListTile.js
@@ -0,0 +1,112 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Typography,
+  Button,
+  ButtonGroup,
+  IconButton,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  CardActionArea,
+} from "@material-ui/core";
+
+import AddRoundedIcon from "@material-ui/icons/AddRounded";
+import RemoveRoundedIcon from "@material-ui/icons/RemoveRounded";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  actions: {
+    display: "flex",
+    alignItems: "flexEnd",
+  },
+  count: {
+    paddingTop: "4px",
+  },
+}));
+
+export default function ShopProductListTile({
+  id,
+  title,
+  description,
+  price,
+  stock,
+  unit,
+  category,
+  imageSrc,
+  count,
+  onChangeCount,
+}) {
+  const classes = useStyles();
+
+  const handleClick = (n) => {
+    const nBag = isNaN(count) ? 0 : count;
+    const updatedCount = nBag + n < 0 ? 0 : nBag + n;
+    onChangeCount(
+      {
+        id,
+        title,
+        description,
+        price,
+        stock,
+        unit,
+        category,
+        imageSrc,
+        count,
+      },
+      updatedCount
+    );
+  };
+
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia className={classes.media} image={imageSrc} />
+        <CardContent>
+          <Typography gutterBottom variant="h6" component="h2">
+            {title}
+          </Typography>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="p"
+            noWrap
+          >
+            {`Rs. ${price}/${unit}`}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions disableSpacing>
+        {count > 0 ? (
+          <ButtonGroup variant="outlined" color="primary">
+            <IconButton size="small" onClick={() => handleClick(-1)}>
+              <RemoveRoundedIcon />
+            </IconButton>
+            <Typography className={classes.count} align="center">
+              {count || 0}
+            </Typography>
+            <IconButton size="small" onClick={() => handleClick(1)}>
+              <AddRoundedIcon />
+            </IconButton>
+          </ButtonGroup>
+        ) : (
+          <Button
+            variant="text"
+            color="primary"
+            startIcon={<AddRoundedIcon />}
+            onClick={() => handleClick(1)}
+          >
+            Add To Bag
+          </Button>
+        )}
+      </CardActions>
+    </Card>
+  );
+}
