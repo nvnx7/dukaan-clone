04628ed3856fde1f43f245bb9209aedbfe2937a7
Naveen <thenvnsahu@gmail.com>
2021-01-08 20:10:50 +0530

    plugged customer reducer

--- frontend/src/reducers/rootReducer.js
+++ frontend/src/reducers/rootReducer.js
@@ -4,12 +4,14 @@ import authReducer from "./authReducers";
 import dashboardReducer from "./dashboardReducers";
 import shopReducer from "./shopReducers";
 import orderReducer from "./orderReducers";
+import customerReducer from "./customerReducers";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   dashboard: dashboardReducer,
   shop: shopReducer,
   order: orderReducer,
+  customer: customerReducer,
 });
 
 export default rootReducer;
