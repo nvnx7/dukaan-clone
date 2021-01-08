8e3354d70b4c110845e58ff7ee181853abb189dd
Naveen <thenvnsahu@gmail.com>
2021-01-08 20:07:05 +0530

    serializer for customer shop access

--- backend/shop/serializers/shop_serializers.py
+++ backend/shop/serializers/shop_serializers.py
@@ -27,3 +27,17 @@ class ShopOwnerSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = ['first_name', 'last_name', 'phone',
                   'date_joined', 'owner_shops']
+
+
+class CustomerShopSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+    url = serializers.HyperlinkedIdentityField(view_name='shop-detail')
+    # owner = HyperlinkedRelatedField(view_name='owner-detail', read_only=True)
+    shop_products = ProductSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Shop
+        fields = ['id', 'url', 'title', 'address', 'date_created',
+                  'shop_products']
+
+        read_only_fields = ['title', 'address', 'date_created']
